808a133a33e6199ab710c4302e16b5867679fb57
Kurt Lourens <hi@kurtlourens.com>
2020-05-24 21:23:31 +0200

    add built items

---
+++ build/precache-manifest.82ffbb283e78a51c43272596e6f91e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02140f5d308070480ddddbb02ecaa3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e773afdb918cb9a08d3",
+    "url": "/static/css/main.03b94846.chunk.css"
+  },
+  {
+    "revision": "c58082cc623824305d4b",
+    "url": "/static/js/2.786dde3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.786dde3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e773afdb918cb9a08d3",
+    "url": "/static/js/main.6b2b1cb6.chunk.js"
+  },
+  {
+    "revision": "f3a4a683c849081a6d9a",
+    "url": "/static/js/runtime-main.ea9e6678.js"
+  }
+]);
